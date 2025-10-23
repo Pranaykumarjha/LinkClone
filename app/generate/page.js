e89f273a0c775ae12daee7cfc96e7ceed9a21248
Pranaykumarjha <pranayjha952@gmail.com>
2025-10-23 18:05:19 +0530

    Fix /generate: disable SSR, use dynamic import for client

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,5 +1,8 @@
-"use client";
-import GenerateClient from "./GenerateClient";
+// app/generate/page.js
+import dynamic from "next/dynamic";
+
+// Force this page to render only on client
+const GenerateClient = dynamic(() => import("./GenerateClient"), { ssr: false });
 
 export default function Page() {
   return <GenerateClient />;
